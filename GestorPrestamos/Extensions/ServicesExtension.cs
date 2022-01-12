ff8eb7ce0dd80eeea275771156b3ca77e5ba9f3c
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-01-12 15:28:56 -0500

    Add Project Configuration

--- GestorPrestamos/Extensions/ServicesExtension.cs
+++ GestorPrestamos/Extensions/ServicesExtension.cs
@@ -1,10 +1,16 @@
-﻿namespace GestorPrestamos.Extensions
+﻿using GestorPrestamos.Data.Implementations.Excel;
+using GestorPrestamos.Data.Interfaces;
+using GestorPrestamos.Services;
+using GestorPrestamos.Services.Interfaces;
+
+namespace GestorPrestamos.Extensions
 {
     public static class ServicesExtension
     {
         public static void RegisterMyCustomServices(this IServiceCollection services)
         {
-            //services.AddScoped<>
+            services.AddScoped<IMyService, MyService>();
+            services.AddScoped<IRepository, ExcelRepository>();
         }
     }
 }
